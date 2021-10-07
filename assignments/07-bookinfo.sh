956df5aaae7c1dc1d2b324621b02af2a3eb70f05
Thanawat Udchachon <thanawat.ud@mail.kmutt.ac.th>
2021-10-07 19:21:57 +0000

    Update README.md

--- assignments/07-bookinfo.sh
+++ assignments/07-bookinfo.sh
@@ -1,4 +1,5 @@
-# Rating service
+## Rating service
+```bash
 docker build -t ratings .
 
 docker run -d --name mongodb -p 27017:27017 \
@@ -6,20 +7,27 @@ docker run -d --name mongodb -p 27017:27017 \
 
 docker run -d --name ratings -p 8080:8080 --link mongodb:mongodb \
   -e SERVICE_VERSION=v1 -e 'MONGO_DB_URL=mongodb://mongodb:27017/ratings' ratings
+```
 
-# Details service
+## Details service
+```bash
 docker build -t details .
 
 docker run -d --name details -p 8081:8081 details
+```
 
-# Review service
+## Review service
+```bash
 docker build -t reviews .
 
 docker run -d --name reviews -p 8082:8082 --link ratings:ratings \
     -e ENABLE_RATINGS=true -e 'RATINGS_SERVICE=http://ratings:8080' reviews
+```
 
-# Productpage service
+## Productpage service
+```bash
 docker build -t productpage .
 
 docker run -d --name productpage -p 8083:8083 --link details:details --link ratings:ratings --link reviews:reviews \
     -e 'DETAILS_HOSTNAME=http://details:8081' -e 'RATINGS_HOSTNAME=http://ratings:8080' -e 'REVIEWS_HOSTNAME=http://reviews:9080' productpage
+```
